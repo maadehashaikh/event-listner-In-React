bd7a5dec95b8fb5c5512cbe4bfac3e0fe7fda466
Maadeha shaikh <maadeham@gmail.com>
2024-04-15 23:48:55 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Buttons from './Buttons'
+
+function App() {
+    return(
+        <>
+        <h1>Hello World !</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quae nobis corporis eum debitis iusto tempore qui hic temporibus officiis odio fugiat, sunt magni porro.</p>
+        <Buttons/>
+        </>
+    )
+}
+export default App
---
+++ src/Buttons.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Buttons() {
+  return (
+    <button>buttons</button>
+  )
+}
+
+export default Buttons
